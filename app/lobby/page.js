7137104c863dcbdecb440ffab7631f6c1df7ce2f
Panuseng <seng.panuwit@gmail.com>
2024-05-04 23:10:52 +0700

    s

--- app/lobby/page.js
+++ app/lobby/page.js
@@ -58,7 +58,7 @@ export default function LobbyPage() {
 							{/* Add more buttons for other platforms ... */}
 						</div>
 					</div>
-						<a href="/lobby/create" className="text-center bg-violet-500 text-white px-4 py-2 rounded-md focus:outline-none focus:ring-violet-300 hover:bg-violet-700">
+						<a href="" className="text-center bg-violet-500 text-white px-4 py-2 rounded-md focus:outline-none focus:ring-violet-300 hover:bg-violet-700">
 							Start Game
 						</a>
 				</div>
